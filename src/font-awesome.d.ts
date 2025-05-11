f6a21179dba3929c9d491bd0dcbe8814792c59c2
“Qchinagit config --global user.email “1303849129@qq.com <13678578+qchina@user.noreply.gitee.com>
2025-05-11 19:48:25 +0800

    20250426

--- src/font-awesome.d.ts
+++
@@ -1,9 +0,0 @@
-// font-awesome.d.ts
-import 'vue';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-declare module '@vue/runtime-core' {
-  export interface GlobalComponents {
-    FontAwesomeIcon: typeof FontAwesomeIcon;
-  }
-}
